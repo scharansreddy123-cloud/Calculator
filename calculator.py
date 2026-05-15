5fe8716234167883d384d226953aec5d700d75ed
scharansreddy123-cloud <scharansreddy123@gmail.com>
2026-05-15 12:30:52 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,4 @@
-Python 3.14.5 (v3.14.5:5607950ef23, May 10 2026, 07:38:09) [Clang 21.0.0 (clang-2100.0.123.102)] on darwin
-Enter "help" below or click "Help" above for more information. 
-num1=int(input("Enter first number: "))
+ num1=int(input("Enter first number: "))
 num2=int(input("Enter second number: "))
  
 operation=input("Choose +,-,*,/: ")
