7f520c4c22113722a50f781274e53ae0bd290dc6
scharansreddy123-cloud <scharansreddy123@gmail.com>
2026-05-15 12:29:12 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,16 +1,17 @@
 Python 3.14.5 (v3.14.5:5607950ef23, May 10 2026, 07:38:09) [Clang 21.0.0 (clang-2100.0.123.102)] on darwin
-Enter "help" below or click "Help" above for more information.
->>> num1=int(input("Enter first number: "))
-... num2=int(input("Enter second number: "))
-... 
-... operation=input("Choose +,-,*,/: ")
-... 
-... if operation=="+":
-...     print(num1+num2)
-... elif operation=="-":
-...     print(num1-num2)
-... elif operation=="*":
-...     print(num1*num2)
-... elif operation=="/":
-...     print(num1/num2)
-... else:
+Enter "help" below or click "Help" above for more information. 
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+ 
+operation=input("Choose +,-,*,/: ")
+ 
+if operation=="+":
+     print(num1+num2)
+elif operation=="-":
+     print(num1-num2)
+elif operation=="*":
+     print(num1*num2)
+elif operation=="/":
+     print(num1/num2)
+else:
+     pritn("invalid operation")
