b42f43569b65aa55850e44a8da05b0e8c4962831
scharansreddy123-cloud <scharansreddy123@gmail.com>
2026-06-01 06:21:23 +0530

    Enhance calculator with new operations and result tracking
    Added support for '%of' and 'abs' operations and updated result handling.

--- calculator.py
+++ calculator.py
@@ -1,24 +1,34 @@
 num1=int(input("Enter first number: "))
 num2=int(input("Enter second number: "))
  
-operation=input("Choose +,-,*,/,**,%,//,sqrt: ")
+operation=input("Choose +,-,*,/,**,%,//,sqrt,%of,abs: ")
  
 if operation=="+":
-     print(num1+num2)
+     result=num1+num2
 elif operation=="-":
-     print(num1-num2)
+     result=num1-num2
 elif operation=="*":
-     print(num1*num2)
+     result=num1*num2
 elif operation=="/":
-     print(num1/num2)
+     result=num1/num2
 elif operation=="**":
-     print(num1**num2)
+     result=num1**num2
 elif operation=="%":
-     print(num1%num2)
+     result=num1%num2
 elif operation=="//":
-     print(num1//num2)
+     result=num1//num2
 elif operation=="sqrt":
      print("Square root of first number:", math.sqrt(num1))
      print("Square root of second number:", math.sqrt(num2))
+     result="Square root Calculated"
+elif operation=="%of":
+     result=num1*num2/100
+elif operation=="abs":
+     result=abs(num1)
 else:
      print("invalid operation")
+     result=None
+ if result is not None:
+    print("Result:", result)
+    history.append(result)
+    print("History:", history)
